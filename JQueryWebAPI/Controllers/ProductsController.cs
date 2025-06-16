e80beaae88aa56a04e2d3a957cc759900c9a8b23
Chris. <125774793+NotEvilChris6629@users.noreply.github.com>
2025-06-16 12:10:23 +0100

    Started work on a WebAPI JQuery project. It currently does not work and I'm trying to figure out why.

---
+++ JQueryWebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,46 @@
+using JQueryWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JQueryWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        Product[] products = new Product[]
+        {
+            new Product {Id = 1, Name = "Bacon", Catagory = "Food", Price = 100},
+            new Product {Id = 2, Name = "yo-yo", Catagory = "Toys", Price = 2.5M},
+            new Product {Id = 3, Name = "Sonic Screwdriver", Catagory = "Hardware", Price = 148M}
+        };
+
+        /// <summary>
+        /// Returns all the products in the array above.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return products;
+        }
+        
+        /// <summary>
+        /// Searches the products using an ID.
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult<Product> Get(int id)
+        {
+            //This searches through the products and returns the first match that satisfies a condition. Returns null if no match is found. This is a Lambda Expression
+            //is essentially a foreach loop written in a much more condensed way.
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
